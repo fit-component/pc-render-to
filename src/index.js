3ef47609ffd616cbea7817acceaef103aef471e4
huangqi03 <huangqi03@baidu.com>
2016-04-19 15:21:22 +0800

    quick push

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+var render_to_1 = require('./render-to');
+exports.RenderTo = render_to_1["default"];
+exports.__esModule = true;
+exports["default"] = render_to_1["default"];
---
+++ src/render-to/module.js
@@ -0,0 +1,14 @@
+"use strict";
+var Props = (function () {
+    function Props() {
+        this.selector = 'body';
+    }
+    return Props;
+}());
+exports.Props = Props;
+var State = (function () {
+    function State() {
+    }
+    return State;
+}());
+exports.State = State;
